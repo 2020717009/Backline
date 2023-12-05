88211c0ba3192bee1b6168d6bb841ec78547a43c
2020717009 <2020717009@ogr.oku.edu.tr>
2023-12-05 21:17:00 +0300

    Initial Commit
    Map Layout
    Path points for enemy to follow
    Enemy spawner

---
+++ Backline/Assets/Scripts/Enemy_Movement.cs
@@ -0,0 +1,16 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        public GameObject nextPoint = GameObject.Find("Point1");
+    }
+
+}
+    
+
