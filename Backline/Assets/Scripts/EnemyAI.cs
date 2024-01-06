e4519fde0c946b901de533d0b88af76a45fde9d1
Yunusemretasar <tasaremree1@gmail.com>
2024-01-06 10:06:05 +0300

    Düşman Yapay Zekası
    Düşman yapay zekasına saldırı gücü başlangıçtaki can değerleri eklendi

--- Backline/Assets/Scripts/EnemyAI.cs
+++ Backline/Assets/Scripts/EnemyAI.cs
@@ -7,10 +7,16 @@ public class EnemyAI : MonoBehaviour
     private Transform hedef;
     public float hiz;
     public float durmaMesafesi;
+    public int baslangicSaglik = 10; // Baþlangýç saðlýk deðeri
+    public int saldiriGucu = 2; // Düþman saldýrý gücü
+
+    private int currentSaglik; // Mevcut saðlýk deðeri
+
     // Start is called before the first frame update
     void Start()
     {
         hedef = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        currentSaglik = baslangicSaglik;
     }
 
     void EnemyFollow()
@@ -23,5 +29,32 @@ public class EnemyAI : MonoBehaviour
     void Update()
     {
         EnemyFollow();
+        // Düþmanýn hareketi veya diðer dinamik davranýþlar burada güncellenebilir
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ates") // Ates tag'ini kullandým, siz kendi oyununuzun kurallarýna göre ayarlayabilirsiniz.
+        {
+            Vur();
+        }
+    }
+
+    void Vur()
+    {
+        currentSaglik--;
+
+        Debug.Log("Düþmanýn kalan saðlýk deðeri: " + currentSaglik);
+
+        if (currentSaglik <= 0)
+        {
+            DusmanOldu();
+        }
+    }
+
+    void DusmanOldu()
+    {
+        Debug.Log("Düþman öldü!");
+        Destroy(gameObject);
     }
 }
